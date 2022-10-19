fdb4f1cd4d3d652a0fd84329ecdd20e786c1dbac
Jakob Wolitzki <jakob.wolitzki@gmail.com>
2022-10-19 17:44:21 +0200

    Fixed admin

--- monitoring/admin.py
+++ monitoring/admin.py
@@ -8,7 +8,7 @@ from monitoring.models import (
 
 class DataRowAdmin(admin.ModelAdmin):
     list_display = ["asin", "created_at", 'listing_price', 'shipping_price', 'seller_feedback_count', 'seller_positive_feedback_rating', 'is_buy_box_winner',]
-    search_fields = ["asin"]
+    search_fields = ["asin__asin"]
     list_filter = ["is_buy_box_winner", 'asin']
 
 
